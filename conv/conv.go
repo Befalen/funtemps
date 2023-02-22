fc9c397f8c71fa59d11e41b48681ca0ea0c4a9dc
Befalen <joakimnormann@yahoo.com>
2023-02-22 21:16:38 +0100

    Grammatikk

--- conv/conv.go
+++ conv/conv.go
@@ -1,25 +1,5 @@
 package conv
 
-/*
-  I denne pakken skal alle konverteringfunksjonene
-  implementeres. Bruk engelsk.
-    FarhenheitToCelsius
-    CelsiusToFahrenheit
-    KelvinToFarhenheit
-    ...
-*/
-
-/*
-// Konverterer Farhenheit til Celsius
-func FarhenheitToCelsius(value float64) float64 {
-	// Her skal du implementere funksjonen
-	// Du skal ikke formattere float64 i denne funksjonen
-	// Gjør formattering i main.go med fmt.Printf eller
-	// lignende
-	return 56.67
-	}
-*/
-
 // fahr til cel
 func FahrenheitToCelsius(fahrenheit float64) float64 {
 	return (fahrenheit - 32) * 5 / 9
